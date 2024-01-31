38af95cbfe28e37d84c0b20818251a0a2ff491b3
xxxjahlilallenxxx <jahlilallen@gmail.com>
2024-01-31 12:22:40 -0500

    Fix [#2] removed MongoDBRepositories annotation in main

--- src/main/java/com/jallenllc/mongodbspringdemo/MongoDbSpringDemoApplication.java
+++ src/main/java/com/jallenllc/mongodbspringdemo/MongoDbSpringDemoApplication.java
@@ -2,10 +2,8 @@ package com.jallenllc.mongodbspringdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class MongoDbSpringDemoApplication {
 
 	public static void main(String[] args) {
